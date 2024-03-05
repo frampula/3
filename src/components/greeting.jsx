74ddcc03a0f6323362592b2114981ffe66a77e89
framupla <kidalo1337@gmail.com>
2024-03-05 18:54:19 +0200

    Очень тяжело, очень...

---
+++ src/components/greeting.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Greeting extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+
+        }
+    }
+    render() {
+        return <h1>Hello {this.props.name}</h1>
+    }
+}
+
+export default Greeting;
