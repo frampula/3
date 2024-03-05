197edda6098b7cbc5bab7f755eda07458718d1bc
framupla <kidalo1337@gmail.com>
2024-03-05 19:29:32 +0200

    Aloha refactor...

--- src/components/Aloha.js
+++ src/components/Aloha.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class Greeting extends React.Component {
+class Aloha extends React.Component {
     constructor(props) {
         super(props);
 
@@ -23,4 +23,4 @@ class Greeting extends React.Component {
     }
 }
 
-export default Greeting;
+export default Aloha;
