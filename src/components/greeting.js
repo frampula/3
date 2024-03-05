38057731f475626d2b9071f3e1115304140956eb
framupla <kidalo1337@gmail.com>
2024-03-05 19:18:09 +0200

    Greeting state, ОЧЕНЬ ТЯЖЕЛО ПОПЕ БОЛЬНО

---
+++ src/components/greeting.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class Greeting extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isGreeting: true
+        }
+    }
+    
+    clickHandler() {
+        this.setState({
+            isGreeting: !this.state.isGreeting
+        })
+    }
+
+    render() {
+
+        const greetingText = this.state.isGreeting ? 'Hello': 'Bye'
+
+        return <h1 onClick={() => {this.clickHandler()}}>{greetingText} {this.props.name}</h1>
+    }
+}
+
+export default Greeting;
