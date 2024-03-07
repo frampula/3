0e062257f8b959353e3c07b9ee3c0f742e5f62bc
framupla <kidalo1337@gmail.com>
2024-03-07 19:35:48 +0200

    Child to parent

---
+++ src/components/Scene/Scene.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Toggler from "./Toggler";
+import "./style.css";
+
+class Scene extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLight: true,
+    };
+  }
+
+  toggleScene() {
+    this.setState({
+      isLight: !this.state.isLight,
+    });
+  }
+
+  render() {
+    const { isLight } = this.state;
+    return (
+      <div className={isLight ? "scene-on" : "scene-off"}>
+        <Toggler changeParentState={() => {this.toggleScene()}} />
+      </div>
+    );
+  }
+}
+
+export default Scene;
