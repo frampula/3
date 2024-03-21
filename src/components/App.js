2a90af9a77bfe39750925c081fa1c40155d0aab4
framupla <kidalo1337@gmail.com>
2024-03-21 19:34:08 +0200

    Callback patter with Context

--- src/components/App.js
+++ src/components/App.js
@@ -10,15 +10,24 @@ class App extends React.Component {
       user: {
         firstName: "John",
         lastName: "Doe",
-        emil: "johndoe@gmail.com",
+        email: "johndoe@gmail.com",
         avatar:
           "https://i.pinimg.com/564x/52/b5/db/52b5db69f1a3073c263f6f7d22a37c3d.jpg",
       },
     };
   }
 
+  logOut = () => {
+    this.setState ({
+      user: {}
+    })
+  }
+
   render() {                                                                                                                                                                                                                                                                                                                                                          
-    return (<UserContext.Provider value={this.state.user}>
+    return (<UserContext.Provider value={{
+      user: this.state.user,
+      logOut: this.logOut
+      }}>
     <Tree />
     </UserContext.Provider>);
   }
--- src/components/Tree/Parent/Subparent/Child/InnerChild/InnerChild.js
+++ src/components/Tree/Parent/Subparent/Child/InnerChild/InnerChild.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { UserContext } from '../../../../../../contexts/userContext';
+import React from "react";
+import { UserContext } from "../../../../../../contexts/userContext";
 
 const InnerChild = (props) => {
-    return (
-        <UserContext.Consumer>
-            {(value) => {
-                return (
-                    <p>{JSON.stringify(value)}</p>
-                )
-            }}
-        </UserContext.Consumer>
-    );
-}
+  return (
+    <UserContext.Consumer>
+      {({user: {firstName, lastName, email, avatar}, logOut}) => {
+        return (
+          <>
+            <button onClick={logOut}>LogOut</button>
+            <p>{firstName} {lastName} {email}</p>
+          </>
+        );
+      }}
+    </UserContext.Consumer>
+  );
+};
 
 export default InnerChild;
