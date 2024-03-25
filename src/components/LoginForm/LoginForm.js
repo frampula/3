b1a0c938870a504c2afe23d18c9e8041afceb204
framupla <kidalo1337@gmail.com>
2024-03-25 19:25:11 +0200

    Formik  & Yup (Validation, Error message)

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import * as yup from "yup";
 import SIGN_UP_SCHEMA from "../../schemas";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const initialState = {
   email: "",
@@ -17,14 +17,18 @@ function SignUpForm(props) {
   };
 
   return (
-    <Formik initialValues={initialState} onSubmit={handleSubmitToFormik}>
+    <Formik initialValues={initialState} onSubmit={handleSubmitToFormik} validationSchema={SIGN_UP_SCHEMA}>
       {(formikProps) => {
         return (
-          <Form>
+          <Form style={{display: 'flex', flexDirection: 'column'}}>
             <Field name="firstName" placeholder="Type ur first name" />
+            <ErrorMessage name="firstName" component="p"/>
             <Field name="lastName" placeholder="Type ur last name"/>
+            <ErrorMessage name="lastName" component="p"/>
             <Field name="email" placeholder="Type ur email"/>
+            <ErrorMessage name="email" component="p"/>
             <Field name="password" placeholder="Type ur password"/>
+            <ErrorMessage name="password" component="p"/>
             <button type="submit">Send nudes</button>
           </Form>
         );
