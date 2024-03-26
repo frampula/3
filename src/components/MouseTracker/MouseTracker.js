11ea6e5888eab0d9ebf1dab1b348fdf521f39017
framupla <kidalo1337@gmail.com>
2024-03-26 19:21:01 +0200

    Best practise

--- src/components/MouseTracker/MouseTracker.js
+++ src/components/MouseTracker/MouseTracker.js
@@ -1,51 +1,42 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const MouseTracker = (props) => {
-    const [coordinates, setCoordinates] = useState({
-        x: 0,
-        y: 0,
-        counter: 0
+  const [coordinates, setCoordinates] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    window.addEventListener("mousemove", trackerHandler);
+
+    return () => {
+      window.removeEventListener("mousemove", trackerHandler);
+    };
+  }, []);
+
+  const trackerHandler = (event) => {
+    const { clientX, clientY } = event;
+
+    setCoordinates({
+        x: clientX,
+        y: clientY
     });
+  };
 
-    useEffect(() => {
-        window.addEventListener('mousemove', trackerHandler);
+  const clickHandler = () => {
+    setCounter(counter + 1);
+  };
 
-        return () => { 
-            window.removeEventListener('mousemove', trackerHandler);
-        }
-
-        
-    }, []); 
-
-    const trackerHandler = (event) => {
-        const { clientX, clientY } = event;
-
-        setCoordinates((prevState) => {
-            return {
-                ...prevState,
-                x: clientX,
-                y: clientY
-            }
-        });
-    }
-
-    const clickHandler = () => {
-        setCoordinates((prevState) => {
-            return {
-                ...prevState,
-                counter: coordinates.counter + 1
-            }
-        });
-    }
-
-    return (
-        <>
-            <p>X: {coordinates.x}</p>
-            <p>Y: {coordinates.y}</p>
-            <button onClick={clickHandler}>Click me</button>
-            <p>Counter: {coordinates.counter}</p>
-        </>
-    );
-}
+  return (
+    <>
+      <p>X: {coordinates.x}</p>
+      <p>Y: {coordinates.y}</p>
+      <button onClick={clickHandler}>Click me</button>
+      <p>Counter: {counter}</p>
+    </>
+  );
+};
 
 export default MouseTracker;
