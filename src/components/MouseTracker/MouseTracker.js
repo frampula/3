94be65a4344bef01b209e86dd3c940950351098c
framupla <kidalo1337@gmail.com>
2024-03-26 19:14:55 +0200

    Hook setstate.. chto

--- src/components/MouseTracker/MouseTracker.js
+++ src/components/MouseTracker/MouseTracker.js
@@ -1,35 +1,51 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const MouseTracker = (props) => {
-  const [coordinates, setCoordinates] = useState({
-    x: 0,
-    y: 0,
-  });
+    const [coordinates, setCoordinates] = useState({
+        x: 0,
+        y: 0,
+        counter: 0
+    });
 
-  useEffect(() => {
-    window.addEventListener("mousemove", trackerHandler);
+    useEffect(() => {
+        window.addEventListener('mousemove', trackerHandler);
 
-    return () => {
-        window.removeEventListener('mousemove', trackerHandler)
+        return () => { 
+            window.removeEventListener('mousemove', trackerHandler);
+        }
+
+        
+    }, []); 
+
+    const trackerHandler = (event) => {
+        const { clientX, clientY } = event;
+
+        setCoordinates((prevState) => {
+            return {
+                ...prevState,
+                x: clientX,
+                y: clientY
+            }
+        });
     }
 
-  }, []);
+    const clickHandler = () => {
+        setCoordinates((prevState) => {
+            return {
+                ...prevState,
+                counter: coordinates.counter + 1
+            }
+        });
+    }
 
-  const trackerHandler = (event) => {
-    const { clientX, clientY } = event;
-
-    setCoordinates({
-      x: clientX,
-      y: clientY,
-    });
-  };
-
-  return (
-    <>
-      <p>X: {coordinates.x}</p>
-      <p>Y: {coordinates.y}</p>
-    </>
-  );
-};
+    return (
+        <>
+            <p>X: {coordinates.x}</p>
+            <p>Y: {coordinates.y}</p>
+            <button onClick={clickHandler}>Click me</button>
+            <p>Counter: {coordinates.counter}</p>
+        </>
+    );
+}
 
 export default MouseTracker;
